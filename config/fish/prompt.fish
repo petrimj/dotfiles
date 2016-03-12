c1bb4f371ca6ec0f9a329c187d14c517723c10ca
Onni Hakala <onni@koodimonni.fi>
2016-03-12 23:24:23 +0200

    Added better terminal theme

--- config/fish/prompt.fish
+++ config/fish/prompt.fish
@@ -19,12 +19,18 @@ function fish_prompt
   # docs: https://github.com/sjl/z-fish
   z --add "$PWD"
 
+  # Quite nice source for colors: http://colours.neilorangepeel.com/
   # Show prompt
   set -l git_branch (git branch ^/dev/null | sed -n '/\* /s///p')
-  echo -n (whoami)'@'(hostname)':'
-  set_color green
+  set_color normal
+  echo -n (whoami)
+  set_color "#FFDAB9" #peachpuff
+  echo -n '@'
+  set_color normal
+  echo -n (hostname)':'
+  set_color "#E9967A" #darksalmon
   echo -n (prompt_pwd)
-  set_color blue
+  set_color "#6495ED" #cornflowerblue
   echo -n '{'"$git_branch"'} '
   set_color normal
   echo -n '> '
