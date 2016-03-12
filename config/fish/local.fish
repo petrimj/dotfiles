c8d3d9cbca353b665189b13e39613ca67d4cf776
Onni Hakala <onni@koodimonni.fi>
2016-03-12 02:26:53 +0200

    Fixed sublime aliases

--- config/fish/local.fish
+++ config/fish/local.fish
@@ -1,10 +1,10 @@
 # Add aliases
 ### Added sublime editor as main editor
-set SUBL '/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
-alias subl '/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
+set SUBL "/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
+alias subl '/usr/local/bin/subl'
 
 ### Sometimes I mistake if I'm in remote or not
-alias rmate '/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
+alias rmate '/usr/local/bin/subl'
 
 # Set default editor
 set -U EDITOR subl
