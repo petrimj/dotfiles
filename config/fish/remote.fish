bb4256deef4d953000642431eedac3bf63dd25c2
Onni Hakala <onni@koodimonni.fi>
2016-03-13 23:44:01 +0200

    Returned rmate hacks because official way didn't work

--- config/fish/remote.fish
+++ config/fish/remote.fish
@@ -2,8 +2,12 @@
 # This file contains commands which are useful only in remote servers
 ##
 
+##
+# Use custom rmate port to avoid clashes with other users
+##
 if command_exists rmate
-  alias subl "rmate"
+  alias subl "rmate --port 52777"
+  alias rmate "rmate --port 52777"
 end
 
 # Use nano as editor in remotes
