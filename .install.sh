c85067293f619cb76c38ee98e4848db2e79397c2
Onni Hakala <onni@koodimonni.fi>
2016-03-13 15:31:14 +0200

    Started using spectacle instead of divvy

--- .install.sh
+++ .install.sh
@@ -4,3 +4,6 @@ brew install rcm aspell wifi-password cowsay # Last one is must have
 
 # Install pygmentize
 pip install pygments --upgrade
+
+# Install spectale for window resizing
+brew cask install spectacle
