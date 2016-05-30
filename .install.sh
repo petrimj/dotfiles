2e638852b85c2843817f958230a70f167cc230a0
Onni Hakala <onni@keksi.io>
2016-05-30 10:33:41 +0300

    Install php70 & composer in install script

--- .install.sh
+++ .install.sh
@@ -29,6 +29,22 @@ brew tap thoughtbot/formulae
 brew tap caskroom/cask
 brew install rcm aspell ansible wifi-password fish node docker gpg2 thefuck cowsay  # Last one is must have
 
+# Install PHP7
+brew tap homebrew/versions
+brew tap homebrew/homebrew-php
+brew unlink php56
+brew install php70
+
+# Install Composer
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '070854512ef404f16bac87071a6db9fd9721da1684cd4589b1196c3faf71b9a2682e2311b36a5079825e155ac7ce150d') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+
+if [ -f composer.phar ];
+  mv composer.phar /usr/local/bin/composer
+fi
+
 # Replace OSX utilities with newer ones
 brew tap homebrew/dupes
 brew install openssh --with-keychain-support
