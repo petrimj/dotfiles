e7ae64e733a2d2e002d27e0dde2b1679f78e5a95
Petri Mäki-Jaakkola <petri.maki-jaakkola@geniem.com>
2017-01-29 11:15:02 +0200

    color changes

--- config/fish/prompt.fish
+++ config/fish/prompt.fish
@@ -75,8 +75,8 @@ set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate 'yes'
 set __fish_git_prompt_showuntrackedfiles 'yes'
 set __fish_git_prompt_showupstream 'yes'
-set __fish_git_prompt_color_branch yellow
-set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_branch green
+set __fish_git_prompt_color_upstream_ahead yellow
 set __fish_git_prompt_color_upstream_behind red
 
 # Status Chars
